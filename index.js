c71cd6c8f433ebf9fe98196aca16c69ae33cbc4e
Darlan Alves <me@darlanalv.es>
2020-08-22 00:55:09 +0200

    feat: upgrade to v2

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 import { lambda, Format } from '@node-lambdas/core';
 
 const configutation = {
-  version: 1,
-  input: Format.Buffer,
+  version: 2,
+  actions: {
+    encode: {
+      default: true,
+      input: Format.Buffer,
+      output: Format.Text,
+      handler: (input, output) => output.send(input.body.toString('base64')),
+    },
+
+    decode: {
+      input: Format.Text,
+      output: Format.Buffer,
+      handler: (input, output) => output.send(Buffer.from(input.body, 'base64')),
+    }
+  }
 };
 
-lambda(configutation, (input, output) => {
-  switch (input.url) {
-    case '/':
-    case '/encode':
-      output.send(Buffer.from(input.body).toString('base64'));
-      break;
-
-    case '/decode':
-      output.send(Buffer.from(input.body.toString('utf8'), 'base64'));
-      break;
-
-    default:
-      output.reject();
-  }
-});
+lambda(configutation);
